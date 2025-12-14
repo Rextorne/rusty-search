36d4211df960a6e501fc70cea7ecf360a1339e79
Nico Rudolph <nico.rudolph@ost.ch>
2025-12-14 18:09:04 +0100

    added search through directory functionality

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,46 @@
+use anyhow::Result;
+use clap::Parser;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-// use std::path::Path;
+use std::path::Path;
+use walkdir::WalkDir;
 
-fn main() -> std::io::Result<()> {
-    let path = "src/test.txt";
-    let word = "Rust";
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
 
     println!("--- Rusty-Search v0.1 ---");
-    println!("Suche nach '{}' in '{}'", word, path);
+    println!("Suche nach '{}' in: {:?}\n", args.pattern, args.path);
 
+    for entry in WalkDir::new(&args.path) {
+        let entry = match entry {
+            Ok(e) => e,
+            Err(_) => continue,
+        };
+
+        if entry.file_type().is_file() {
+            search_in_file(entry.path(), &args.pattern)?;
+        }
+    }
+
+    Ok(())
+}
+
+// --- 3. Die Such-Logik (Hier kommst du ins Spiel) ---
+fn search_in_file(path: &Path, pattern: &str) -> Result<()> {
     let file = File::open(path).expect("Unable to open file");
 
     let reader = BufReader::new(file);
 
     for line in reader.lines() {
         let line = line?;
-        if line.contains(word) {
-            println!("Found word in Line '{}'", line);
+        if line.contains(pattern) {
+            println!("Found pattern in File '{:?}'", path);
         }
     }
 
