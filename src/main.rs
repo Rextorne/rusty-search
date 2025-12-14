07dd5191ba958c8975380776627e849d96b98b67
Nico Rudolph <nico.rudolph@ost.ch>
2025-12-14 17:20:07 +0100

    search text in file works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+// use std::path::Path;
+
+fn main() -> std::io::Result<()> {
+    let path = "src/test.txt";
+    let word = "Rust";
+
+    println!("--- Rusty-Search v0.1 ---");
+    println!("Suche nach '{}' in '{}'", word, path);
+
+    let file = File::open(path).expect("Unable to open file");
+
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let line = line?;
+        if line.contains(word) {
+            println!("Found word in Line '{}'", line);
+        }
+    }
+
+    Ok(())
 }
